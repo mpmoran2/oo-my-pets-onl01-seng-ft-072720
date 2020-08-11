277eaefd03f129f743a09c080438118898341fd1
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 18:45:18 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,17 +1,4 @@
 class Cat
-  attr_reader :name, :owner
-  attr_accessor :mood
-
-  def initialize(name)
-      @name = name
-      @owner = owner
-      @mood = "nervous"
-      @@all << self
-      @cats = []
-  end
   
-  def self.all
-    @all
-  end
 end
 
