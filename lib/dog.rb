52fb30eb7c00595ec3d27dd3efb971fd59eb0336
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 18:45:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,3 @@
 class Dog
-  attr_reader :name, :owner
-  attr_accessor :mood
-
-  def initialize(name)
-    @name = name
-      @owner = owner
-      @mood = "nervous"
-      @@all << self
-      @dogs = []
-  end
   
-  def self.all
-    @all
-  end
 end
